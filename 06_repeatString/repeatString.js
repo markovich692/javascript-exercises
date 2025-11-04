50996e41cada5f55728e14705305d54ebdfd435c
Marc Diallo <mark.devguru@outlook.com>
2025-11-04 17:32:02 -0600

    Implement repeatString function and pass all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const number = Math.floor(Math.random() * 1000);
 
+const repeatString = function (string, number) {
+  let str = "";
+  if (number < 0) return "ERROR";
+  for (let i = 0; i < number; i++) {
+    str += string;
+  }
+  return str;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
