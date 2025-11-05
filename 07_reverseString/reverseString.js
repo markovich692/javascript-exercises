ac0825c28d11bf25675fc7a21bd3ea565d8eb45f
Marc Diallo <mark.devguru@outlook.com>
2025-11-04 20:40:24 -0600

    Define the reverseString() and pass all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splittedString = string.split("");
+  return splittedString.reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
