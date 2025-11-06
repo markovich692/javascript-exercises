e83e19cac5627811eb8e18ac960e94175cdf34b4
Marc Diallo <mark.devguru@outlook.com>
2025-11-06 10:37:01 -0600

    Modify removeFromArray() to use rest operator for flexibility
    Using the rest operator allows all extra arguments to be combined
    into a single array, making it easier to loop through and remove
    each matching value from the main array efficiently.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,25 +1,14 @@
-const removeFromArray = function (arr, index, number = 1) {
-  let count = 0;
-  let indexes = [];
-
-    if (!arr.includes(index)) return arr;
-
-  arr.forEach(function (item, i) {
-    if (item === index) {
-      count += 1;
-      indexes.push(i);
-    } else {
-      count += 0;
+const removeFromArray = function (arr, ...entries) {
+  entries.forEach(function (entry) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (entry === arr[i]) {
+        arr.splice(i, 1);
+      } else {
+        continue;
+      }
     }
   });
 
-  if (count >= 2) {
-    for (let i = indexes.length - 1; i >= 0; i--) {
-      arr.splice(indexes[i], 1);
-    }
-  } else {
-    arr.splice(index - number, number);
-  }
   return arr;
 };
 
