906bc1b5d5daa952f5ef1b50ca73e88d71ffb5cf
Marc Diallo <mark.devguru@outlook.com>
2025-11-06 16:07:23 -0600

    Add condition to accept only positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (...entries) {
+  let arrValues = [];
+  let sum = 0;
 
+  let condition = entries.some((num) => num < 0 || !Number.isInteger(num));
+
+  if (condition) return "ERROR";
+
+  //Sort the array of entries from smallest to largest value
+  entries.sort((a, b) => a - b);
+
+  //Store entries into variables start and end
+  let [start, end] = entries;
+
+  for (let i = start; i <= end; i++) {
+    arrValues.push(i);
+  }
+
+  arrValues.forEach((num) => {
+    sum += num;
+  });
+
+  return sum;
 };
 
 // Do not edit below this line
