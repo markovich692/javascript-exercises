1defdd59f13fb10977a13c4c0e18ebe325694837
Marc Diallo <mark.devguru@outlook.com>
2025-11-05 21:37:26 -0600

    Fix removeFromArray() to handle multiple values and remove duplicates

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, index, number = 1) {
+  let count = 0;
+  let indexes = [];
+
+  arr.forEach(function (item, i, array) {
+    if (item === index) {
+      count += 1;
+      indexes.push(i);
+    } else {
+      count += 0;
+    }
+  });
+
+  if (count >= 2) {
+    for (let i = indexes.length - 1; i >= 0; i--) {
+      arr.splice(indexes[i], 1);
+    }
+  } else {
+    arr.splice(index - number, number);
+  }
+  return arr;
 };
 
 // Do not edit below this line
