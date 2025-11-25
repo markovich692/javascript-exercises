e6c95dd213ae94871d1ec4fca50eef665a7e6c32
Marc Diallo <mark.devguru@outlook.com>
2025-11-24 18:30:31 -0600

    Define add, substract, sum, multiply, power, factorial functions and pass all tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return Number(a) + Number(b);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  a = Array.isArray(a) ? a.reduce((acc, num) => (acc += num), 0) : a;
+
+  return Number(a);
 };
 
-const multiply = function() {
+const multiply = function (a) {
+  a = Array.isArray(a) ? a.reduce((acc, num) => (acc *= num), 1) : a;
 
+  return Number(a);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let initialArr = [1];
+
+  for (let i = 1; i <= a; i++) {
+    initialArr.push(i);
+  }
+
+  const res = initialArr.reduce((acc, num) => (acc *= num));
+  return res;
 };
 
-// Do not edit below this line
 module.exports = {
   add,
   subtract,
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
