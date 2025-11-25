fd3a1ea94b9de19068f6a69c281d3ca9299ce72b
Marc Diallo <mark.devguru@outlook.com>
2025-11-25 10:44:24 -0600

    Define palindromes() function and pass all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const removeChar = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
+  const reverseChar = removeChar.split("").reverse().join("");
+
+  return removeChar === reverseChar ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
