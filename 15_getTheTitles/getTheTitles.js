d7f79077822d177ab9fce3319dcf196d13c54014
Marc Diallo <mark.devguru@outlook.com>
2025-11-25 17:20:27 -0600

    Define the getTheTitles() function and pass all tests

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function (booksArray) {
+  const res = booksArray.map(function (book) {
+    return book.title;
+  });
 
+  return res;
 };
 
 // Do not edit below this line
