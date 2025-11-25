e7073f58df88881158fd4cd1b4e11aba39e74193
Marc Diallo <mark.devguru@outlook.com>
2025-11-25 12:51:20 -0600

    Define the fibonacci() function and pass all tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  let arr = [0, 1];
 
+  number = Number(number);
+
+  if (number === 0) return 0;
+
+  if (number < 0) return "OOPS";
+
+  for (let i = 2; i <= number; i++) {
+    let arrLength = arr.length;
+
+    let value = arr[arrLength - 2] + arr[arrLength - 1];
+
+    arr.push(value);
+  }
+
+  return arr[arr.length - 1];
 };
 
-// Do not edit below this line
+//Do not edit below this line
 module.exports = fibonacci;
