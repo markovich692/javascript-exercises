522d1d4763f3ae1b99f3d25c84b5a5b2ead23c5b
Marc Diallo <mark.devguru@outlook.com>
2025-11-25 18:49:06 -0600

    Define the findTheOldest() function and pass all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayOfPeople) {
+  let oldestPerson;
+  const arrOldestToYoungestBirth = arrayOfPeople.sort((a, b) => a.yearOfBirth - b.yearOfBirth);
 
+  if (arrOldestToYoungestBirth[0].yearOfDeath === undefined) {
+    oldestPerson = arrOldestToYoungestBirth[0];
+  } else {
+    findOldestPerson = arrayOfPeople
+      .filter(function (person) {
+        return person.yearOfDeath != undefined;
+      })
+      .sort(function (a, b) {
+        return b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth);
+      });
+    oldestPerson = findOldestPerson[0];
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
